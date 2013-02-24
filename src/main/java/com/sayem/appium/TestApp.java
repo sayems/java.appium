f875167a62cbf182125653bafa012805659832e8
Syed Sayem <syed@hotmail.com>
2013-02-24 13:07:00 -0500

    Initial commit

---
+++ src/main/java/com/sayem/appium/TestApp.java
@@ -0,0 +1,73 @@
+package com.sayem.appium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestApp {
+
+    private WebDriver driver;
+
+    private List<Integer> values;
+
+    private static final int MINIMUM = 0;
+    private static final int MAXIMUM = 10;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        // set up appium
+        File app = new File("/Users/sayem/appium-apps/TestApp.app");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "iOS");
+        capabilities.setCapability(CapabilityType.VERSION, "6.0");
+        capabilities.setCapability(CapabilityType.PLATFORM, "Mac");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        values = new ArrayList<Integer>();
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+
+    private void populate() {
+        //populate text fields with two random number
+        List<WebElement> elems = driver.findElements(By.tagName("textField"));
+        Random random = new Random();
+        for (WebElement elem : elems) {
+            int rndNum = random.nextInt(MAXIMUM - MINIMUM + 1) + MINIMUM;
+            elem.sendKeys(String.valueOf(rndNum));
+            values.add(rndNum);
+        }
+    }
+
+    @Test
+    public void testUIComputation() throws Exception {
+
+        // populate text fields with values
+        populate();
+        // trigger computation by using the button
+        WebElement button = driver.findElement(By.tagName("button"));
+        button.click();
+        // is sum equal ?
+        WebElement texts = driver.findElement(By.tagName("staticText"));
+        assertEquals(texts.getText(), String.valueOf(values.get(0) + values.get(1)));
+    }
+
+
+}

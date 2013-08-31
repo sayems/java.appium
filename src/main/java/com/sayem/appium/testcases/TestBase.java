be3236af894c4004493f892e042f8323b28135b8
Syed Sayem <syed@hotmail.com>
2013-08-31 11:06:57 -0400

    Added initConfigurations method

--- src/main/java/com/sayem/appium/testcases/TestBase.java
+++ src/main/java/com/sayem/appium/testcases/TestBase.java
@@ -3,6 +3,7 @@ package com.sayem.appium.testcases;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
+import java.io.FileInputStream;
 import java.util.Properties;
 
 public class TestBase {
@@ -10,4 +11,19 @@ public class TestBase {
     public static Properties CONFIG=null;
     public static WebDriver driver=null;
     public static boolean isLoggedIn=false;
+
+    public void initConfigurations(){
+        if(CONFIG==null){
+            // Logging
+            APPLICATION_LOGS = Logger.getLogger("devpinoyLogger");
+            // config.prop
+            CONFIG = new Properties();
+            try {
+                FileInputStream fs = new FileInputStream("Appium/src/main/java/com/sayem/config/config.properties");
+                CONFIG.load(fs);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }

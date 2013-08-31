3db684c047745dbdd0116a9ab58b885f8c3c2f0d
Syed Sayem <syed@hotmail.com>
2013-08-31 10:46:24 -0400

    Added methods to HomePage

--- src/main/java/com/sayem/appium/pages/HomePage.java
+++ src/main/java/com/sayem/appium/pages/HomePage.java
@@ -3,6 +3,7 @@ package com.sayem.appium.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
@@ -27,5 +28,28 @@ public class HomePage {
     @FindBy(name = "Settings")
     private WebElement SettingsButton;
 
+    public MyMoneyPage goToMyMoneyPage(){
+        myMoneyButton.click();
+        return PageFactory.initElements(driver, MyMoneyPage.class);
+    }
 
+    public MyGoalPage goToMyGoalPage(){
+        myReadsButton.click();
+        return PageFactory.initElements(driver, MyGoalPage.class);
+    }
+
+    public MyReadsPage goToMyReadsPage(){
+        myReadsButton.click();
+        return PageFactory.initElements(driver, MyReadsPage.class);
+    }
+
+    public MyExpertPage goToMyExpertPage(){
+        myExpertButton.click();
+        return PageFactory.initElements(driver, MyExpertPage.class);
+    }
+
+    public SettingsPage goToSettingsPage(){
+        SettingsButton.click();
+        return PageFactory.initElements(driver, SettingsPage.class);
+    }
 }

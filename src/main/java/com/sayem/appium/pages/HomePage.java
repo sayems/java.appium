b9f91c8bb6504d25f35f3b6fb9e47e46ca21ab7c
Syed Sayem <syed@hotmail.com>
2013-08-31 10:42:49 -0400

    Added Constructor & WebDriver field

--- src/main/java/com/sayem/appium/pages/HomePage.java
+++ src/main/java/com/sayem/appium/pages/HomePage.java
@@ -1,4 +1,12 @@
 package com.sayem.appium.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
 }

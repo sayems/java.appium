4529cbb6d613f4766a31a84504e578c700cf3b76
Syed Sayem <syed@hotmail.com>
2013-08-31 11:06:24 -0400

    Added static field

--- src/main/java/com/sayem/appium/testcases/TestBase.java
+++ src/main/java/com/sayem/appium/testcases/TestBase.java
@@ -1,4 +1,13 @@
 package com.sayem.appium.testcases;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
 public class TestBase {
+    public static Logger APPLICATION_LOGS = null;
+    public static Properties CONFIG=null;
+    public static WebDriver driver=null;
+    public static boolean isLoggedIn=false;
 }

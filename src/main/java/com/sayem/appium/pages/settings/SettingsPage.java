004451a49c4f0278ded524acd0bd302460dead62
Syed Sayem <syed@hotmail.com>
2013-09-01 10:19:21 -0400

    Added WebElements for settings page

--- src/main/java/com/sayem/appium/pages/settings/SettingsPage.java
+++ src/main/java/com/sayem/appium/pages/settings/SettingsPage.java
@@ -1,6 +1,8 @@
 package com.sayem.appium.pages.settings;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class SettingsPage {
 
@@ -10,4 +12,22 @@ public class SettingsPage {
     public SettingsPage(WebDriver driver){
         this.driver = driver;
     }
+
+    @FindBy(name = "PASSCODE")
+    private WebElement passcodeButton;
+
+    @FindBy(name = "ACCOUNTS")
+    private WebElement accountsButton;
+
+    @FindBy(name = "HELP & SUPPORT")
+    private WebElement helpAndSupportButton;
+
+    @FindBy(name = "FEEDBACK")
+    private WebElement feedbackButton;
+
+    @FindBy(name = "LOG OUT")
+    private WebElement  logoutButton;
+
+    @FindBy(xpath = "//window[3]/alert[1]/button[2]")
+    private WebElement alertBox;
 }

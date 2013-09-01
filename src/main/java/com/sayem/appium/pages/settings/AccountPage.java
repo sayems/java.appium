47c49126cdf312f7751ade51234bf99e7deae6d5
Syed Sayem <syed@hotmail.com>
2013-09-01 10:22:37 -0400

    Added @FindBy WebElements to connect to account

--- src/main/java/com/sayem/appium/pages/settings/AccountPage.java
+++ src/main/java/com/sayem/appium/pages/settings/AccountPage.java
@@ -20,4 +20,27 @@ public class AccountPage {
 
     @FindBy(xpath = "//window[1]/tableview[1]/cell[1]/text[1]")
     private WebElement popUpAccountSearchName;
+
+    //    Connect to account username and password
+
+
+    @FindBy(xpath = "//window[1]/tableview[1]/cell[1]/textfield[1]")
+    //@FindBy(name = "Username")
+    private WebElement userName; // locate by name: Username
+
+    @FindBy(xpath = "//window[1]/tableview[1]/cell[2]/secure[1]")
+    private WebElement password; // locate by name: Password
+
+    @FindBy(xpath = "//window[1]/tableview[1]/group[2]/button[1]")
+    private WebElement cancelButton; // locate by name: Cancel
+
+    @FindBy(xpath = "//window[1]/tableview[1]/group[2]/button[2]")
+    private WebElement submitButton; // locate by name: Submit
+
+    @FindBy(xpath = "//window[1]/toolbar[1]/button[1]")
+    private WebElement backButton; // locate by name: Back
+
+
+    @FindBy(xpath = "//window[2]/toolbar[1]/button[3]")
+    private WebElement doneButton; // locate by name: Done
 }

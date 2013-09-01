adf56c74cff495582c34bbfcb2e896ef5ca651db
Syed Sayem <syed@hotmail.com>
2013-09-01 10:22:00 -0400

    Added @FindBy WebElements for accountPlusSign, searchForAnAccount & popUpAccountSeachName

--- src/main/java/com/sayem/appium/pages/settings/AccountPage.java
+++ src/main/java/com/sayem/appium/pages/settings/AccountPage.java
@@ -1,6 +1,8 @@
 package com.sayem.appium.pages.settings;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class AccountPage {
 
@@ -9,4 +11,13 @@ public class AccountPage {
     public AccountPage(WebDriver driver){
         this.driver = driver;
     }
+
+    @FindBy(xpath = "//window[1]/toolbar[1]/button[2]")
+    private WebElement accountPlusSign;
+
+    @FindBy(xpath = "//window[1]/textfield[1]")
+    private WebElement searchForAnAccount;
+
+    @FindBy(xpath = "//window[1]/tableview[1]/cell[1]/text[1]")
+    private WebElement popUpAccountSearchName;
 }

f78fad780148940dd1a1ae1ed88454e703caaa5a
Syed Sayem <syed@hotmail.com>
2013-09-01 10:20:59 -0400

    Added constructor and WebDriver field

--- src/main/java/com/sayem/appium/pages/settings/AccountPage.java
+++ src/main/java/com/sayem/appium/pages/settings/AccountPage.java
@@ -1,4 +1,12 @@
 package com.sayem.appium.pages.settings;
 
+import org.openqa.selenium.WebDriver;
+
 public class AccountPage {
+
+    private WebDriver driver;
+
+    public AccountPage(WebDriver driver){
+        this.driver = driver;
+    }
 }

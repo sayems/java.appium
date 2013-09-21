5a771149ab326a9ef50a2a9bf642eb158ddf25f7
Syed Sayem <syed@hotmail.com>
2013-09-21 12:00:01 -0400

    Improved MyExppertPage class

--- src/main/java/com/sayem/appium/pages/myexpert/MyExpertPage.java
+++ src/main/java/com/sayem/appium/pages/myexpert/MyExpertPage.java
@@ -1,4 +1,24 @@
 package com.sayem.appium.pages.myexpert;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MyExpertPage {
+
+    private WebDriver driver;
+
+    public MyExpertPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//window[1]/button[1]")
+    private WebElement signUpLoginButton;
+
+    public SignupPage signUpAndLogin(){
+        signUpLoginButton.click();
+        return PageFactory.initElements(driver, SignupPage.class);
+
+    }
 }

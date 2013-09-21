d013424a39d9ccb7638ff7032b1f3ce18f2c54db
Syed Sayem <syed@hotmail.com>
2013-09-21 12:02:14 -0400

    Added LandingPage class

---
+++ src/main/java/com/sayem/appium/testcases/LandingPage.java
@@ -0,0 +1,24 @@
+package com.sayem.appium.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LandingPage {
+
+    private WebDriver driver;
+
+    public LandingPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//window[1]/button[1]")
+    private WebElement signUpLoginButton;
+
+    public SignupPage signUpAndLogin(){
+        signUpLoginButton.click();
+        return PageFactory.initElements(driver, SignupPage.class);
+
+    }
+}

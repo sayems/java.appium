0e7f19c020030d0c79451b9a1101dbc65d6079fe
Syed Sayem <syed@hotmail.com>
2013-09-22 11:54:05 -0400

    Improved TestLogin class

--- src/main/java/com/sayem/appium/testcases/TestLogin.java
+++ src/main/java/com/sayem/appium/testcases/TestLogin.java
@@ -1,4 +1,27 @@
 package com.sayem.appium.testcases;
 
-public class TestLogin {
+import com.sayem.appium.base.TestBase;
+import com.sayem.appium.pages.LandingPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestLogin extends TestBase {
+
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        initDriver();
+    }
+
+
+    @Test
+    protected void testUIComputation() throws Exception {
+
+        String emailAddress = "sayem@ilearnvest.com";
+        String password = "user1234";
+
+        LandingPage landingPage = PageFactory.initElements(driver, LandingPage.class);
+        landingPage.signUpAndLogin().login().loginIn(emailAddress, password);
+    }
 }
